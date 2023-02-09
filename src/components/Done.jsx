af96173656ac3e669a51b74cd971ed6953010285
junhyuck park <wnsgur1855@naver.com>
2023-02-09 23:40:28 +0900

    todolist Done 컴포넌트

--- src/components/Done.jsx
+++ src/components/Done.jsx
@@ -1,6 +1,24 @@
-const Done = ({ item, doneDel, clickfalseButtonHandler }) => {
+const Done = ({ item, done, setDone, todo, setTodo, index }) => {
+  const doneDel = (id) => {
+    setDone(done.filter((item) => item.id !== id));
+  };
+  const clickfalseButtonHandler = (item) => {
+    const find = done.filter((value) => value.id !== item.id);
+    setDone(find);
+    setTodo([
+      ...todo,
+      {
+        id: item.id,
+        title: item.title,
+        content: item.content,
+        isDone: !item.done,
+      },
+    ]);
+    alert('완료');
+  };
+  console.log(todo, done);
   return (
-    <div key={item.id} className="componentStyle">
+    <div key={index} className="componentStyle">
       <div className="boxTitle">{item.title}</div>
       <h6 className="boxContent">{item.content}</h6>
       <button onClick={() => doneDel(item.id)} className="deleteButton">
@@ -8,10 +26,9 @@ const Done = ({ item, doneDel, clickfalseButtonHandler }) => {
       </button>
       &nbsp;
       <button onClick={() => clickfalseButtonHandler(item)} className="completeButton">
-        done추가
+        취소
       </button>
     </div>
   );
 };
-
 export default Done;
