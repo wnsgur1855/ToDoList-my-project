547de63a050a3873173fa2f473446e24d74d0564
junhyuck park <wnsgur1855@naver.com>
2023-02-09 23:40:42 +0900

    todolist Input 컴포넌트

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,22 @@
-const Input = ({ title, content, handleContentChange, handleTitleChange, handleAdd }) => {
+const Input = ({ setTitle, setContent, setTodo, todo, title, content }) => {
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+  const handleContentChange = (event) => {
+    setContent(event.target.value);
+  };
+  const handleAdd = () => {
+    setTodo([...todo, { id: todo.length + 1, title: title, content: content, isDone: false }]);
+    alert('추가');
+  };
+
   return (
     <div className="title">
-      제목
+      제목 &nbsp;
       <input value={title} onChange={handleTitleChange} className="inputter" type="text" /> &nbsp;
-      내용
-      <input value={content} onChange={handleContentChange} className="inputter" type="text" />
+      내용 &nbsp;
+      <input value={content} onChange={handleContentChange} className="inputter" type="text" />{' '}
+      &nbsp;
       <button onClick={handleAdd} className="Buttonman">
         추가
       </button>
